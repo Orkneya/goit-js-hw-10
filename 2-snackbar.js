71e553642a6c0921afd012dd78e3acb808226c76
Orkneya <orkneya17@gmail.com>
2025-02-06 22:43:48 +0000

    Deploying to gh-pages from @ Orkneya/goit-js-hw-10@48f33b751f0d184c6e3e9145c7d186dba73f7ac7 🚀

---
+++ 2-snackbar.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */import{i as o}from"./assets/vendor-A92OCY9B.js";o.show({message:"Please choose a date in the future",position:"topRight",messageColor:"white",backgroundColor:"red"});o.show({message:"Please choose a date in the future",position:"topRight",messageColor:"white",backgroundColor:"green"});
+//# sourceMappingURL=2-snackbar.js.map
